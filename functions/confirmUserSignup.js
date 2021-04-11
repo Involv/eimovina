28c832cf912a8bd5e77c826af71739ce0098fb2a
milunradonjic <milun.radonjic95@gmail.com>
2021-04-11 19:10:56 +0200

    update user

--- functions/confirmUserSignup.js
+++ functions/confirmUserSignup.js
@@ -3,10 +3,12 @@ import dynamoDb from "../libs/dynamodb-lib";
 export const main = async (event) => {
   if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
     const name = event.request.userAttributes['name'];
+    const now = new Date().toJSON();
     const user = {
       id: event.userName,
       name,
-      createdAt: new Date().toJSON(),
+      createdAt: now,
+      lastUpdatedAt: now,
     };
 
     const params = {
---
+++ functions/updateUser.js
@@ -0,0 +1,20 @@
+import dynamoDb from "../libs/dynamodb-lib";
+
+export const main = async (event) => {
+  const params = {
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id: event.arguments.userId,
+    },
+    UpdateExpression: "SET lastUpdatedAt = :now",
+    ExpressionAttributeValues: {
+      ":now": new Date().toJSON()
+    },
+    ConditionExpression: 'attribute_exists(id)',
+    ReturnValues: "ALL_NEW",
+  };
+
+  const updatedUser = await dynamoDb.update(params);
+  console.log("UPDATED: ", updatedUser);
+  return updatedUser.Attributes;
+};
