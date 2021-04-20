65b9dbddf94e39b86ab33747ee2c04387c323c19
Ban Markovic <ban.markovic@infinum.hr>
2021-04-20 08:07:37 +0200

    Update PropertiesLoad script with ES6 format and fixed mapping of fields

--- eimovina-be/PropertiesLoadData.js
+++ eimovina-be/PropertiesLoadData.js
@@ -1,4 +1,4 @@
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk")
 const request = require("request-promise").defaults({
   jar: true,
   followAllRedirects: true
@@ -6,44 +6,46 @@ const request = require("request-promise").defaults({
 
 AWS.config.update({
   region: "eu-central-1",
-});
+})
 
-const docClient = new AWS.DynamoDB.DocumentClient();
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-async function main() {
+const municipality = {
+  id: 16,
+  name: "PODGORICA",
+  submunicipalityId: 55,
+  submunicipalityName: "PODGORICA III",
+}
+
+const main = async () => {
   const result = await scrapeData()
-
-  let mappedData = mapData(result)
-
+  const mappedData = mapData(result)
   putToDynamoDb(mappedData)
 }
 
-function putToDynamoDb(properties) {
-  properties.forEach(function (property) {
-    docClient.put(property, function (err, data) {
+const putToDynamoDb = properties => {
+  properties.forEach(async (property) => {
+    await docClient.put(property, (err, data) => {
       if (err) {
-        console.error("Unable to add property. Error JSON:", JSON.stringify(err, null, 2));
+        console.error("Unable to add property. Error JSON:", JSON.stringify(err, null, 2))
       } else {
-        console.log("PutItem succeeded");
+        console.log("PutItem succeeded ", property)
       }
-    });
-  });
+    })
+  })
 }
 
-function mapData(data) {
-  let params = []
-  data.forEach(function (item) {
-    params.push(mapProperties(item.searchResult))
-  });
-
+const mapData = data => {
+  const params = []
+  data.forEach(item => params.push(mapProperties(item.searchResult)))
   return params
 }
 
-function mapProperties(data) {
-  let params = {
-    TableName: "eimovina-be-dev-PropertyTable-13945OFDRGG8G",
+const mapProperties = data => {
+  const params = {
+    TableName: "eimovina-be-dev-PropertyTable-3Q89I5IQCB3J",
     Item: {
-      id: String(Date.now()),
+      id: "",
       realEstateListNumber: "",
       plotNumber: "",
       address: "",
@@ -52,27 +54,46 @@ function mapProperties(data) {
       objects: [],
       loans: [],
       municipalityId: "",
-      submunicipalityId: ""
+      municipalityName: "",
+      submunicipalityId: "",
+      submunicipalityName: "",
     }
   }
-  for (const property in data) {
-    params.Item.realEstateListNumber = data["list"].broj_lista,
-    params.Item.plotNumber = data["list"].broj_parcele || "",
-    params.Item.address = data["deloviParcela"].rows[0].adresa,
-    params.Item.rightHolders = mapRightHolders(data["vlasniciZemljista"].rows),
-    params.Item.plotParts = mapPlotParts(data["deloviParcela"].rows),
-    params.Item.objects = mapObjects(data["objekti"].rows),
-    params.Item.loans = mapLoans(data["tereti"].rows),
-    params.Item.municipalityId = data["list"].oznaka_kat_opstine,
-    params.Item.submunicipalityId = data["list"].oznaka_kat_opstine
+
+  const {list, deloviParcela} = data
+
+  let plotNumber = list.broj_parcele?.trim()
+  if (!plotNumber && deloviParcela?.rows?.length > 0) {
+    plotNumber = deloviParcela.rows[0].broj
   }
+
+  let address = ""
+  if (deloviParcela?.rows?.length > 0) {
+    address = deloviParcela?.rows[0].adresa
+  }
+
+  params.Item.realEstateListNumber = list.broj_lista
+  params.Item.plotNumber = plotNumber || ""
+  params.Item.address = address
+  params.Item.rightHolders = mapRightHolders(data.vlasniciZemljista.rows)
+  params.Item.plotParts = mapPlotParts(deloviParcela.rows)
+  params.Item.objects = mapObjects(data.objekti.rows)
+  params.Item.loans = mapLoans(data.tereti.rows)
+  params.Item.municipalityId = list.poid
+  params.Item.municipalityName = municipality.name
+  params.Item.submunicipalityId = list.koid
+  params.Item.submunicipalityName = list.naziv_kat_opstine
+
+  params.Item.id = `${params.Item.municipalityId}/` +
+                    `${params.Item.submunicipalityId}/` +
+                    `${params.Item.realEstateListNumber}`
 
   return params
 }
 
-function mapRightHolders(data) {
+const mapRightHolders = data => {
   result = []
-  data.forEach(function (item) {
+  data.forEach(item => {
     result.push({
       name: item.imePrezime,
       rightsScope: item.udeo,
@@ -83,9 +104,9 @@ function mapRightHolders(data) {
   return result
 }
 
-function mapPlotParts(data) {
+const mapPlotParts = data => {
   result = []
-  data.forEach(function (item) {
+  data.forEach(item => {
     result.push({
       usagePurpose: item.nacinKoriscenja,
       address: item.address,
@@ -102,15 +123,15 @@ function mapPlotParts(data) {
   return result
 }
 
-function mapObjects(data) {
+const mapObjects = data => {
   result = []
-  data.forEach(function (item) {
+  data.forEach(item => {
     result.push({
       usagePurpose: item.nacinKoriscenja,
-      objectNumber: item.broj,
+      objectNumber: item.pd,
       roomCount: item.soba,
       address: item.adresa,
-      buildingNumber: item.buildingid,
+      buildingNumber: item.redniBroj,
       area: item.povrsina,
       storey: item.spratnost,
       basisOfAcquisition: item.osnov
@@ -120,56 +141,56 @@ function mapObjects(data) {
   return result
 }
 
-function mapLoans(data) {
+const mapLoans = data => {
   result = []
-  data.forEach(function (item) {
+  data.forEach(item => {
     result.push({
       usagePurpose: item.nacinKoriscenja,
       loanNumber: item.brojTereta,
-      buildingNumber: item.buildingid,
-      serialNumber: item.nss,
-      description: "",
+      buildingNumber: item.redniBroj,
+      serialNumber: item.ns,
+      description: item.opis,
     })
   })
 
   return result
 }
 
-async function scrapeData() {
-  let searchResult = []
-  await request.get('https://ekatastar.me/ekatastar-web/action/elogin')
+const scrapeData = async () => {
+  const searchResult = []
+  await request.get("https://ekatastar.me/ekatastar-web/action/elogin")
   await request.post(
-    'https://ekatastar.me/ekatastar-web/action/login',
+    "https://ekatastar.me/ekatastar-web/action/login",
     {
       form: {
-        username: 'KORISNIK',
-        password: 'KORISNIK',
-        opstinaSchema: 'KNZ_PODGORICA',
-        nazivIzabraneOpstine: 'Podgorica'
+        username: "KORISNIK",
+        password: "KORISNIK",
+        opstinaSchema: "KNZ_PODGORICA",
+        nazivIzabraneOpstine: "Podgorica"
       }
     }
   )
 
   for (let plotNumber = 1000; plotNumber < 1003; plotNumber++) {
-    await request.post(
-      'https://ekatastar.me/ekatastar-web/action/search/ajax/katastarNepokretnosti',
-      {
-        form: {
-          opstinaId: 16,
-          nazivOpstine: 'PODGORICA',
-          katastarskaOpstinaId: 55,
-          nazivKatastarskeOpstine: 'PODGORICA III',
-          searchCriteria: 1,
-          list: plotNumber,
-          brojParcele: null
+    try {
+      const resp = await request.post(
+        "https://ekatastar.me/ekatastar-web/action/search/ajax/katastarNepokretnosti",
+        {
+          form: {
+            opstinaId: municipality.id,
+            nazivOpstine: municipality.name,
+            katastarskaOpstinaId: municipality.submunicipalityId,
+            nazivKatastarskeOpstine: municipality.submunicipalityName,
+            searchCriteria: 1,
+            list: plotNumber,
+            brojParcele: null
+          }
         }
-      })
-      .then((res) => {
-        searchResult.push(JSON.parse(res))
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+      )
+      searchResult.push(JSON.parse(resp))
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   return searchResult
