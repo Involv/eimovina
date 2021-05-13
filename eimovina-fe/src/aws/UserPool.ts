804783a48d02efcdf17e8a3affcc2b3c30d2c618
popovicanja <anjapopovic@anjas-mbp-2.t-com.me>
2021-05-14 00:19:33 +0200

    Aws export and user pool

--- eimovina-fe/src/aws/UserPool.ts
+++ eimovina-fe/src/aws/UserPool.ts
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "eu-central-1_xqU2WHgot",
-  ClientId: "51e56bgiaa3d51ep6rc53g7iba",
+  UserPoolId: "eu-central-1_1ekDz8GSm",
+  ClientId: "3lcoqrs8vj6g56p7b5fbavgse0",
 };
 
 export const UserPool = new CognitoUserPool(poolData);
--- eimovina-fe/src/aws/aws-export.ts
+++ eimovina-fe/src/aws/aws-export.ts
@@ -1,9 +1,9 @@
 const awsmobile = {
   aws_appsync_graphqlEndpoint:
-    "https://2tarjvoynbgszeny34f6ceo3ke.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "https://kew2bcug2raenbels3nmkdf3pa.appsync-api.eu-central-1.amazonaws.com/graphql",
   aws_appsync_region: "eu-central-1",
-  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
-  aws_appsync_apiKey: "da2-rm35odiy2bdutgrftqfneupmna",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-d63rofanojezxjip4paxk5szsi",
 };
 
 export default awsmobile;
