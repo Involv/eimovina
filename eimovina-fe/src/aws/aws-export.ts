c6390713bc22e276057d665ea4d469cf6814a7f3
milunradonjic <milun.radonjic95@gmail.com>
2021-05-15 18:59:55 +0200

    configure aws-exports and user pools

--- eimovina-fe/src/aws/aws-export.ts
+++ eimovina-fe/src/aws/aws-export.ts
@@ -1,9 +1,9 @@
 const awsmobile = {
   aws_appsync_graphqlEndpoint:
-    "https://kew2bcug2raenbels3nmkdf3pa.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "https://j3tkoxgrj5acjjzxwalecfndyq.appsync-api.eu-central-1.amazonaws.com/graphql",
   aws_appsync_region: "eu-central-1",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-d63rofanojezxjip4paxk5szsi",
+  aws_appsync_apiKey: "da2-jwc3uamnbjf23mcbzi7rxlkjle",
 };
 
 export default awsmobile;
