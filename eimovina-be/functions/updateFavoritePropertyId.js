dc0791ef7a13e78d995de6da64fce27c84bd029d
Milun Radonjic <56149107+milunradonjic@users.noreply.github.com>
2021-05-15 17:56:13 +0200

    Favorization (#21)
    * reafactor favorization
    
    * refactor favorization

--- eimovina-be/functions/updateFavoritePropertyId.js
+++ eimovina-be/functions/updateFavoritePropertyId.js
@@ -1,104 +1,45 @@
 import dynamoDb from "../libs/dynamodb-lib";
 
 export const main = async (event) => {
-  console.log("EVENT: ", {event});
-
   const { propertyId } = event.arguments;
   const { username } = event.identity;
 
-  const userParams = {
-    TableName: process.env.USERS_TABLE,
-    Key: {
-      id: username,
-    }
-  };
-  const userResp = await dynamoDb.get(userParams);
+  const user = await getItem(username, process.env.USERS_TABLE);
+  const property = await getItem(propertyId, process.env.PROPERTY_TABLE);
 
-  const newFavoritePropertyIds = configureFavoritePropertyIdsArray(
-    propertyId,
-    userResp.Item.favoritePropertyIds
-  );
+  const newFavoritePropertyIds = addOrRemoveFromArray(propertyId, user.favoritePropertyIds);
+  const newFavoriteBy = addOrRemoveFromArray(username, property.favoriteBy);
 
-  const isPropertyFavorite = newFavoritePropertyIds.includes(propertyId);
+  const updatedUser = await updateArray(username, newFavoritePropertyIds, 'favoritePropertyIds', process.env.USERS_TABLE);
+  await updateArray(propertyId, newFavoriteBy, 'favoriteBy', process.env.PROPERTY_TABLE);
 
-  const propertyParams = {
-    TableName: process.env.PROPERTY_TABLE,
-    Key: {
-      id: propertyId,
-    }
-  };
-  const propertyResp = await dynamoDb.get(propertyParams);
-
-  const newFavoriteBy = configureFavoriteByArray(
-    username,
-    propertyResp.Item.favoriteBy,
-    isPropertyFavorite
-  );
-  await updatePropertyFavoriteBy(propertyId, newFavoriteBy);
-
-  const updatedUser = await updateUserFavoriteProperties(username, newFavoritePropertyIds);
   return updatedUser.Attributes;
 };
 
-const updateUserFavoriteProperties = async (userId, newFavoritePropertyIds) => {
-  const updateUserParams = {
-    TableName: process.env.USERS_TABLE,
-    Key: {
-      id: userId,
-    },
-    UpdateExpression: "SET favoritePropertyIds = :propertyIds",
+const getItem = async (id, tableName) => {
+  const params = {
+    TableName: tableName,
+    Key: { id }
+  };
+  const resp = await dynamoDb.get(params);
+  return resp.Item;
+};
+
+const addOrRemoveFromArray = (el, arr = []) => {
+  return arr.includes(el) ? arr.filter(arrEl => arrEl !== el) : [...arr, el];
+};
+
+const updateArray = async (id, newArr = [], arrField, tableName) => {
+  const params = {
+    TableName: tableName,
+    Key: { id },
+    UpdateExpression: `SET ${arrField} = :newArr`,
     ExpressionAttributeValues: {
-      ":propertyIds": newFavoritePropertyIds
+      ":newArr": newArr,
     },
     ConditionExpression: "attribute_exists(id)",
     ReturnValues: "ALL_NEW"
   };
 
-  return await dynamoDb.update(updateUserParams);
-};
-
-const updatePropertyFavoriteBy = async (propertyId, newFavoriteBy) => {
-  const updatePropertyParams = {
-    TableName: process.env.PROPERTY_TABLE,
-    Key: {
-      id: propertyId,
-    },
-    UpdateExpression: "SET favoriteBy = :newFavoriteBy",
-    ExpressionAttributeValues: {
-      ":newFavoriteBy": newFavoriteBy
-    },
-    ConditionExpression: "attribute_exists(id)"
-  };
-  await dynamoDb.update(updatePropertyParams);
-};
-
-const configureFavoritePropertyIdsArray = (propertyId, favoritePropertyIds) => {
-  const newFavoritePropertyIds = !favoritePropertyIds ? [] : [...favoritePropertyIds];
-
-  console.log("passed propertyId: ", propertyId);
-  console.log("BEFORE favoritePropertyIds: ", newFavoritePropertyIds);
-
-  if (newFavoritePropertyIds.includes(propertyId)) {
-    const propertyIdIndex = newFavoritePropertyIds.indexOf(propertyId);
-    newFavoritePropertyIds.splice(propertyIdIndex, 1);
-  } else {
-    newFavoritePropertyIds.push(propertyId);
-  }
-
-  console.log("AFTER favoritePropertyIds: ", newFavoritePropertyIds);
-
-  return newFavoritePropertyIds;
-};
-
-const configureFavoriteByArray = (userId, favoriteBy, isPropertyFavorite) => {
-  const newFavoriteBy = !favoriteBy ? [] : [...favoriteBy];
-
-  if (isPropertyFavorite) {
-    newFavoriteBy.push(userId);
-  } else {
-    const userIdIndex = newFavoriteBy.indexOf(userId);
-    newFavoriteBy.splice(userIdIndex, 1);
-  }
-
-  return newFavoriteBy;
+  return dynamoDb.update(params);
 };
