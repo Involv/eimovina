92bd45054431ccd61fe8f623ef7b2f5612049613
milunradonjic <milun.radonjic95@gmail.com>
2021-05-17 21:13:40 +0200

    change fe exporsts and user pool

--- eimovina-fe/src/aws/UserPool.ts
+++ eimovina-fe/src/aws/UserPool.ts
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "eu-central-1_F7UoTLuzq",
-  ClientId: "bjsmps8q7hcfbep86j7gldg09",
+  UserPoolId: "eu-central-1_GgnG9YNBs",
+  ClientId: "56nv9l8lkos4568dan4n55ivmg",
 };
 
 export const UserPool = new CognitoUserPool(poolData);
--- eimovina-fe/src/aws/aws-export.ts
+++ eimovina-fe/src/aws/aws-export.ts
@@ -1,9 +1,9 @@
 const awsmobile = {
   aws_appsync_graphqlEndpoint:
-    "https://j3tkoxgrj5acjjzxwalecfndyq.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "https://hcywkyriy5csbb3eol4glzngwq.appsync-api.eu-central-1.amazonaws.com/graphql",
   aws_appsync_region: "eu-central-1",
   aws_appsync_authenticationType: "API_KEY",
-  aws_appsync_apiKey: "da2-jwc3uamnbjf23mcbzi7rxlkjle",
+  aws_appsync_apiKey: "da2-sgkwnp6uwbdclmlc436szmpb64",
 };
 
 export default awsmobile;
